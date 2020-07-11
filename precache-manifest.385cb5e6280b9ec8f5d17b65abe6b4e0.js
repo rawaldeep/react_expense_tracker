ce071476e2515495ab534c8451048400151f31a9
rawaldeep <rawaldeepsingh26@gmail.com>
2020-07-12 00:21:02 +0200

    Updates

---
+++ precache-manifest.385cb5e6280b9ec8f5d17b65abe6b4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70ef416ae671b65f27ae01871170cc1",
+    "url": "/react_expense_tracker/index.html"
+  },
+  {
+    "revision": "21ffb5c78fe8bb8ae4f4",
+    "url": "/react_expense_tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/react_expense_tracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_expense_tracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ffb5c78fe8bb8ae4f4",
+    "url": "/react_expense_tracker/static/js/main.1439a69e.chunk.js"
+  },
+  {
+    "revision": "5a650aa9ea5160f32ad8",
+    "url": "/react_expense_tracker/static/js/runtime-main.658847e2.js"
+  }
+]);
